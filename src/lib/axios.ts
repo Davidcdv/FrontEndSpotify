7ac4caefe8e2e085b68b8281900e9dc0a9a05615
Mac <mac@Macs-MacBook-Pro.local>
2024-11-28 11:16:43 +0100

    Initial commit

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,4 +5,5 @@ const apiUrl = import.meta.env.VITE_API_URL;
 
 export const axiosInstance = axios.create({
   baseURL: apiUrl, // Use the API URL from the environment variable
+  withCredentials: true, // Include credentials for secure cookies, if needed
 });
