bde701f3bcfd40fbc0d9fdaf3b851a687d1c1e0d
Mac <mac@Macs-MacBook-Pro.local>
2024-11-28 10:24:47 +0100

    Update frontend with Axios and environment configurations

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+// Get the API URL from the environment variables
+const apiUrl = import.meta.env.VITE_API_URL;
+
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+  baseURL: apiUrl, // Use the API URL from the environment variable
 });
